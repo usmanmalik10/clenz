cfcc01c2b060e79f485867d6a4856eddb33e8710
AbuBaker <abubakertws@gmail.com>
2023-06-18 22:42:07 +0500

    add api

--- Routes/order.js
+++ Routes/order.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { getAvailableSlots, booknow } = require("../Controllers/order")
+const { getAvailableSlots, booknow, getAllpending, getAllCompleted } = require("../Controllers/order")
 const router = express.Router();
 
 router.post('/book-order', booknow)
 router.get('/getslots', getAvailableSlots)
+router.get('/getpending', getAllpending)
+router.get('/getcompleted', getAllCompleted)
 
 
 
