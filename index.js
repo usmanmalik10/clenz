04ee71b3099de0bad51a39dcf327d079cdf3c18c
AbuBaker <abubakertws@gmail.com>
2023-07-06 10:20:17 +0500

    jjj

--- index.js
+++ index.js
@@ -18,10 +18,6 @@ app.use(cors({
     allowedHeaders: '*'
   }));
 
-app.use(function(req, res, next) {
-  res.setHeader('Content-Security-Policy', 'upgrade-insecure-requests');
-  next();
-});
 
 app.use(express.json());
 
