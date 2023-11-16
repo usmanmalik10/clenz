4b4907ce76c87152246f04f4622a0058d512afe8
usmanmalik10 <usmanmaliksahib2611@gmail.com>
2023-11-16 15:09:37 +0500

    commit

--- index.js
+++ index.js
@@ -1,34 +1,38 @@
 require('dotenv').config();
 const express = require('express');
-var cors = require('cors')
+var cors = require('cors');
 const bodyparser = require('body-parser');
 
 const dbconnection = require('./Database');
 const userRouter = require('./Routes/user');
-const order = require('./Routes/order')
+const order = require('./Routes/order');
 // http://18.191.99.9
 
-const port = process.env.PORT
-
+const port = process.env.PORT;
 
 const app = express();
-app.use(cors({
+app.use(
+  cors({
     origin: '*',
     methods: ['POST', 'PUT', 'PATCH', 'GET', 'DELETE', 'OPTIONS'],
-    allowedHeaders: '*'
-  }));
-
+    allowedHeaders: '*',
+  })
+);
 
 app.use(express.json());
-
 
 // DataBase connection
 dbconnection();
 
-
 app.use('/user', userRouter);
-app.use('/order', order)
+app.use('/order', order);
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'this is get api from clenz',
+  });
+});
 
 app.listen(port || 4000, () => {
-    console.log('your server runing at:' + 4000);
+  console.log('your server runing at:' + 4000);
 });
